95ffb370ce27ab300096a195d2ce38685e306c76
rudolphOtoo <rudolphotoo0@gmail.com>
2026-07-01 21:35:33 +0000

    Add favicon and update browser tab title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>WIS-CMS</title>
+    <title>WIS-CMS — Wesleyan International Society</title>
+    <link rel="icon" type="image/png" href="/favicon.png" />
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/main.jsx'])
 </head>
